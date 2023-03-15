8f1fe625ee0c4d91d2234e21c3eb4b56d097eb51
Imasandi <91633499+Imasandi@users.noreply.github.com>
2023-03-15 20:05:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# answer in a random bunch of words , nouns, colors, verbs
+# take all of those words and put them into a story randomly
+"""
+print("Roses are Red")
+print("Violets are blue")
+print("I Love You")
+
+"""
+color = input("Enter a color")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+"""
+print("Roses are {color}")
+print("{plural noun} are blue")
+print("I Love {celebrity}")
+
+"""
+
+# create three variables
+
+print("Roses are " + color)
+print(plural_noun+" are blue")
+print("I Love " + celebrity)
