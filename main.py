37b6944d2b8d138f6bff7e66f5f489c3b11b2c27
Imasandi <91633499+Imasandi@users.noreply.github.com>
2023-03-16 16:49:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-# answer in a random bunch of words , nouns, colors, verbs
-# take all of those words and put them into a story randomly
-"""
-print("Roses are Red")
-print("Violets are blue")
-print("I Love You")
+# story
+# read your own story
+""" a spooky campfire story
+Mad libs """
 
-"""
-color = input("Enter a color")
-plural_noun = input("Enter a plural noun: ")
-celebrity = input("Enter a celebrity: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter your favourite person: ")
+place = input("Enter your favourite place: ")
+piece_of_clothing = input("Enter your favourite cloth: ")
+song = input("Enter your favourite song: ")
+food = input("Enter your favourite food: ")
+smell = input("Enter your favourite smell: ")
+verb = input("Enter your favourite thing to do when you are happy: ")
+feeling = input(" Enter your feeling after you have done something stupid: ")
+print(" It was a " + adjective + " summer day." + noun + " and I were excited to go camping at " + place + "." )
 
-"""
-print("Roses are {color}")
-print("{plural noun} are blue")
-print("I Love {celebrity}")
+print(" It was my first time going there. I packed my favorite " + piece_of_clothing +". I think it was perfect for camping.")
+print("We were listening to " + song + " all the way down.")
+print(" When we got there, I smelled " + food + " has being cooked. It smells like " + smell + ".")
+print(" I " + verb +" in the room where I was staying with my " + noun + " The next thing I knew was that I was " + feeling + ".")
+print("But I was very happy because I am with my " + noun + ".")
 
-"""
-
-# create three variables
-
-print("Roses are " + color)
-print(plural_noun+" are blue")
-print("I Love " + celebrity)
